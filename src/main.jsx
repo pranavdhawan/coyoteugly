307488a68ae04452d5d886556f5c8468e343e04a
Pranav Dhawan <dhawan.pranav8@gmail.com>
2023-12-20 16:23:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -48,7 +48,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-	<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+	<ClerkProvider frontendApi={PUBLISHABLE_KEY}>
 		<RouterProvider router={router} />
 	</ClerkProvider>
 	</React.StrictMode>
